554ac1117b60715f590d5e799a1273320556184e
Seifu Detso <99719558+iwanoszet07@users.noreply.github.com>
2022-03-10 13:20:34 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - Entery ponit
  *
  * Return: Always 0 (success)
  */
-int main()
+int main(int c, char *v[])
 {
-	fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	print("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
