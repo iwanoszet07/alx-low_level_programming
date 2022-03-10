4641ce2f31a7873cd2c31bd7e335b2ee9aaa4ee9
Seifu Detso <99719558+iwanoszet07@users.noreply.github.com>
2022-03-10 13:04:54 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora korpar, 2015-10-19\n");
+	puts("and that piece of art is useful\" - Dora korpar, 2015-10-19\n");
 	return (1);
 }
