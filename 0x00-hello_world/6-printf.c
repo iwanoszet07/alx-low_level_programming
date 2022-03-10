4b1c96ef2cd6742ca287c80d43fea7af55553c50
Seifu Detso <99719558+iwanoszet07@users.noreply.github.com>
2022-03-10 07:25:17 -0800

    Create 6-printf.c

---
+++ 0x00-hello_world/6-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	int ii;
+	long int li;
+	long long int lli;
+	char c;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(ii));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
