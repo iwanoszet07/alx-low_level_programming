5a3dab13a00a946d3a757ea03e7a47b1e5a067a2
Seifu Detso <99719558+iwanoszet07@users.noreply.github.com>
2022-03-15 12:41:39 -0700

    functions_nested_loopsls

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,12 @@
-#include <unistd.h>
-#include "main.h"
+#include <stdio.h>
 /**
  * main - mtd name to impliment
- * Description: It prints the set of characters, followed by a new line.
+ * Description: It print using putchar, followed by a new line.
  * Return: 0.
  */
 int main(void)
 {
-	char setchar[6] = "School";
-	int i;
-
-	for (i = 0; i < 6; i++)
-		_putchar(setchar[i]);
-		_putchar('\n');
-
+	_putchar('');
+	_putchar('\n');
 	return (0);
 }
