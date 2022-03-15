b9f6837c4575bae2ac542dfdd996473c037def91
Seifu Detso <99719558+iwanoszet07@users.noreply.github.com>
2022-03-15 14:07:07 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * add - mtd name to impliment
+ * @a: An int a
+ * @b: An int b
  * Description: Function that adds two integers and returns the result
  * Return: Result to add a and b
  */
