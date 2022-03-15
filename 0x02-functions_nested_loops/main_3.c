b5b9926c47812fdf75f12f3f57bc689561c604b2
Seifu Detso <99719558+iwanoszet07@users.noreply.github.com>
2022-03-15 13:18:30 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/main_3.c
+++ 0x02-functions_nested_loops/main_3.c
@@ -11,6 +11,10 @@ int main(void)
 	
 	r = _islower('H');
 	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
 	_putchar('\n');
-	return (0);
+	return (0);	
 }
