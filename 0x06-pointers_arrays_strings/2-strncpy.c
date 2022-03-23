2cce2c7850ad68a2dedb625283836ee9be2b7ddb
Seifu Detso <99719558+iwanoszet07@users.noreply.github.com>
2022-03-23 12:06:26 -0700

    C-programming pointers_arrays_strings-basics

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,24 +9,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
+int i;
 
-	while (*src)
-	{
-		srclen++;
-		src++;
-	}
-
-	srclen++;
-
-	if (n > srclen)
-		n = srclen;
-
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-
-	return (temp);
+i = 0;
+while (i < n && *(src + i))
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
