1062e97ee68b5b3fcbb03b1d2cbd400d3e5c7e59
Seifu Detso <99719558+iwanoszet07@users.noreply.github.com>
2022-04-13 15:49:33 +0300

    0x0F-function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,34 +2,23 @@
 #include <stdlib.h>
 
 /**
- * main - prints opcode of own main function
- * @argc: argument count
- * @argv: array of arguments
- * Return: 1 or 2 on fail, 0 on success
- */
+* main - print opcodes of its own main function
+* @argc: number of arguments
+* @argv: vector of arguments
+* Return: 0 if no errors
+*/
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	unsigned char *func_ptr;
+register int i, num;
+char *ptr = (char *)main;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	func_ptr = (unsigned char *)main;
-	i = 0;
-	if (bytes > 0)
-	{
-		while (i < (bytes - 1))
-			printf("%02hhx ", func_ptr[i++]);
-		printf("%hhx\n", func_ptr[i]);
-	}
-	return (0);
+if (argc != 2)
+printf("Error\n"), exit(1);
+num = atoi(argv[1]);
+if (num < 0)
+printf("Error\n"), exit(2);
+for (i = 0; i < num - 1; i++)
+printf("%02hhx ", ptr[i]);
+printf("%02hhx\n", ptr[i]);
+return (0);
 }
