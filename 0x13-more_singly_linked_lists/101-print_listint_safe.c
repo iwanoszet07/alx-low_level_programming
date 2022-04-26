19b73d7e806142fb5985db7b883038468d5025f1
Seifu Detso <99719558+iwanoszet07@users.noreply.github.com>
2022-04-26 11:21:18 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,61 @@
 #include "lists.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * print_listint_safe - A function that prints the elementsin a  list
- * @head: A pointer to listint_t structure
- * Return: The number of nodes. Exits with 98 on failure
- */
+* _rm - reallocates memory for an array of pointers
+* to the nodes in a linked list
+* @list: the old list
+* @size: size of the new list
+* @ne: new node
+*
+* Return: pointer to the new list
+*/
+const listint_t **_rm(const listint_t **list, size_t size, const listint_t *ne)
+{
+const listint_t **new_list;
+size_t i;
+
+new_list = malloc(size * sizeof(listint_t *));
+if (new_list == NULL)
+{
+free(list);
+exit(98);
+}
+for (i = 0; i < size - 1; i++)
+new_list[i] = list[i];
+new_list[i] = ne;
+free(list);
+return (new_list);
+}
+
+/**
+* print_listint_safe - prints a listint_t linked list.
+* @head: pointer to the start of the list
+*
+* Return: the number of nodes in the list
+*/
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
-	const listint_t *one = head, *two = head;
+size_t i, count = 0;
+const listint_t **list = NULL;
 
-	if (head == NULL)
-		exit(98);
-
-	while (one && two && two->next && head)
-	{
-		one = one->next;
-		two = two->next->next;
-		if (one == two)
-		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
-		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		nodes++;
-	}
-	head = NULL;
-	return (nodes);
+while (head != NULL)
+{
+for (i = 0; i < count; i++)
+{
+if (head == list[i])
+{
+printf("-> [%p] %d\n", (void *)head, head->n);
+free(list);
+return (count);
+}
+}
+count++;
+list = _rm(list, count, head);
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+free(list);
+return (count);
 }
